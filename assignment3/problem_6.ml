b32d585d3607f173385e7b7bd4dbe200c2192678
harshilg0yal <harshilgoyal17@gmail.com>
2025-01-28 19:33:09 +0530

    Completed Assignment 3

--- assignment3/problem_6.ml
+++ assignment3/problem_6.ml
@@ -2,8 +2,8 @@ type shape =
 | Circle of float
 | Rectangle of float * float;;
 
-let area s = match s with
+let solution s = match s with
 | Circle radius -> 3.14 *. radius *. radius
 | Rectangle (width,height) -> width *. height;;
-(*input ->  let c = Circle 10.0 in area c;;*)
+(*input ->  let c = Circle 10.0 in solution c;;*)
 (*output -> - : float = 314. *)
--- assignment3/problem_8.ml
+++ assignment3/problem_8.ml
@@ -1,48 +1,49 @@
 exception InvalidInput of string
 
-let parse_int_list input_string =
-  let is_digit ch = ch >= '0' && ch <= '9' in
+let is_num (ch: char) : bool = ch >= '0' && ch <= '9'
+let comma = ','
 
-  let rec string_to_int str i acc =
-    if i < 0 then acc
-    else if is_digit str.[i] then
-      string_to_int str (i - 1) (acc * 10 + (Char.code str.[i] - Char.code '0'))
+
+let rec split_delim (delimiter: char) (s: string) (idx: int): string list =
+  let len = String.length s in
+  let rec helper delimiter s idx current_substring result =
+    if idx >= len then
+      List.rev (current_substring :: result)
     else
-      raise (InvalidInput ("Invalid character in input: " ^ String.make 1 str.[i]))
+      match s.[idx] with
+      | c when c = delimiter ->
+          helper delimiter s (idx + 1) "" (current_substring :: result)
+      | c ->
+          helper delimiter s (idx + 1) (current_substring ^ Char.escaped c) result
   in
+  helper delimiter s idx "" []
 
 
-  let rec split_on_comma str i acc current =
-    if i = String.length str then
-      if current <> "" then current :: acc else acc
-    else if str.[i] = ',' then
-      if current = "" then
-        raise (InvalidInput "Misplaced or multiple commas detected")
-      else
-        split_on_comma str (i + 1) (current :: acc) ""
-    else if is_digit str.[i] then
-      split_on_comma str (i + 1) acc (current ^ String.make 1 str.[i])
-    else
-      raise (InvalidInput ("Invalid character in input: " ^ String.make 1 str.[i]))
-  in
-
-  let reversed_strings = split_on_comma input_string 0 [] "" in
+let rec stringlist_to_intlist (lst1: string list) =
+  match lst1 with
+  | [] -> []
+  | h :: t ->
+      if h = "" then raise (InvalidInput "Wrong commas in the input")
+      else (int_of_string h) :: (stringlist_to_intlist t)
 
 
-  let rec convert_to_int_list strs acc =
-    match strs with
-    | [] -> List.rev acc
-    | hd :: tl ->
-      let number = string_to_int hd (String.length hd - 1) 0 in
-      convert_to_int_list tl (number :: acc)
-  in
+let parse_string_to_int (s: string) : int list =
+  let parts = split_delim comma s 0 in
+  stringlist_to_intlist parts
 
-  convert_to_int_list reversed_strings []
-  
-let () =
+let solution  =
+  print_endline "Please enter the string: ";
   try
-    let input = "1,2,3,4" in
-    let int_list = parse_int_list input in
-    List.iter (fun x -> Printf.printf "%d\n" x) int_list
+    let input = read_line () in
+    Printf.printf "Input: %s\n" input;
+    let output = parse_string_to_int input in
+    let output_str = String.concat "; " (List.map string_of_int output) in
+    Printf.printf "Output: [%s]\n" output_str;
+    output 
   with
-  | InvalidInput msg -> Printf.printf "Error: %s\n" msg
+  | InvalidInput msg ->
+      Printf.printf "Error: %s\n" msg;
+      []
+  | Failure _ ->
+      Printf.printf "Error: Invalid input, unable to parse integers\n";
+      []
