546f1b2ef902bdcdd416de64b509fb572bb594fc
harshilg0yal <harshilgoyal17@gmail.com>
2025-01-30 21:30:23 +0530

    Refactoring and some changes in Problem_7.ml in Assignment 3

--- assignment3/problem_7.ml
+++ assignment3/problem_7.ml
@@ -1,14 +1,14 @@
-exception NotFound of int * int
+exception NotFound of int
 
 let rec solution predicate lst =
   let rec length lst = match lst with | [] -> 0 | h::t -> 1 + length t in
   let len = length lst in
   match lst with
-  | [] -> raise (NotFound(0,len))
+  | [] -> raise (NotFound(len))
   | h::t ->
    if predicate h then 0
   else try 1 + solution predicate t with
-  | NotFound (idx,_) -> raise (NotFound(idx+1,len));;
+  | NotFound (idx) -> raise (NotFound(idx+1));;
 
 (*input -> let pred  x = x mod 3 = 0
             solution pred ([1;2;4;5;7;8;9])*)
